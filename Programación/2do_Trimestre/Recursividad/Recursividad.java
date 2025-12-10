6d3c31430211658c61292303c74d2152819e80e2
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2025-12-10 15:43:43 +0100

    Guardado provisional

--- Programación/2do_Trimestre/Recursividad/Recursividad.java
+++ Programación/2do_Trimestre/Recursividad/Recursividad.java
@@ -14,6 +14,9 @@ public class Recursividad{
 
         System.out.println(sumaArray(a, i));
         System.out.println(potencia(x, n));
+        System.out.println(busquedaBinaria(a, i, i, i));
+        System.out.println(fibrec(5));
+        
 
     }
 
@@ -21,65 +24,78 @@ public class Recursividad{
 
     public static int sumaArray(int[] a, int i) {
     
-    if (i >= a.length) {
-    
-        return 0;
-    
+        if (i >= a.length) {
+        
+            return 0;
+        
+        }
+
+        return a[i] + sumaArray(a, i + 1);
+
     }
 
-    return a[i] + sumaArray(a, i + 1);
-    
-    }
-    
-    /* Actividad 2 */
+        /* Actividad 2 */
 
     public static long potencia(long x, long n) {
+        
+        if (n == 0) {
+            return 1;
+        }
+
+        long half = potencia(x, n / 2);
+        long resultado = half * half;
+
+
+        if (n % 2 != 0) {
+            resultado = x * resultado;
+        }
+
+        return resultado;
     
-    if (n == 0) {
-        return 1;
     }
-
-    long half = potencia(x, n / 2);
-    long result = half * half;
-
-    
-    if (n % 2 != 0) {
-        result = x * result;
-    }
-
-    return result;
-}
 
     /* Actividad 3 */
 
     public static int busquedaBinaria(int[] a, int izq, int der, int clave) {
 
-    if (izq > der) {
+        if (izq > der) {
+
+            return -1;
         
-        return -1;
-    
+        }
+
+        int mid = izq + (der - izq) / 2;
+
+        if (a[mid] == clave) {
+        
+            return mid;
+        
+        } 
+
+        else if (clave < a[mid]) {
+        
+            return busquedaBinaria(a, izq, mid - 1, clave);
+        
+        } 
+
+        else {
+        
+            return busquedaBinaria(a, mid + 1, der, clave);
+        
+        }
+
     }
 
-    int mid = izq + (der - izq) / 2;
+    /* Actividad 4 */
 
-    if (a[mid] == clave) {
-    
-        return mid;
-    
-    } 
-    
-    else if (clave < a[mid]) {
-    
-        return busquedaBinaria(a, izq, mid - 1, clave);
-    
-    } 
-    
-    else {
-    
-        return busquedaBinaria(a, mid + 1, der, clave);
-    
+    public static long fibrec(int n){
+        
+        
+        
+        return fibrec(n) + fibrec(n-1);
+
+
+
     }
-
-}
 
 }
