7677709e966f4f50c22c509688813b5b2a928729
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2025-12-15 17:21:41 +0100

    PrimerEjercicioSimulacro

---
+++ Programación/2do_Trimestre/PracticaExamen/SimulacroExamen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SimulacroExamen{
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        //historiaMiedo();
+
+    }
+
+    public static void historiaMiedo(){
+
+        System.out.println("Introduce la fecha en formato DD/MM");
+
+        String introFecha = sc.next();
+        String separador="/";
+        String[] DDMM = introFecha.split(separador);
+        
+        
+
+    }
+
+}
