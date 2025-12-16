6efc1d746421634d1736a9fb7e6b307e04647520
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2025-12-16 16:01:14 +0100

    PrimeraParte ArrayList

--- Programación/2do_Trimestre/ArrayList/ListaArray.java
+++ Programación/2do_Trimestre/ArrayList/ListaArray.java
@@ -35,9 +35,45 @@ public class ListaArray{
 
             }
 
-
-
         } while (true);
+
+        System.out.println();
+        System.out.println(alumnos.isEmpty());
+        System.out.println();
+        System.out.println(alumnos.size());
+        System.out.println();
+        if (!alumnos.isEmpty()) {
+            
+            System.out.println("El primer índice es: " + alumnos.get(0));
+            System.out.println();
+            System.out.println("El segundo índice es: " + alumnos.get(alumnos.size()-1));
+
+            System.out.print("Introduce el nombre del alumno que deseas buscar:");
+            comprobante=sc.next();
+            System.out.println(alumnos.contains(comprobante)); 
+
+            System.out.print("Introduce el numero del alumno que deseas buscar en orden que hayas introducido previamente (ej: 1 = Jesús - 2 = Marta - 3 = Cristian): ");
+            int indice = sc.nextInt();
+            
+                /* "int indiceMax = indice.size()-1;"  Me salta error por lo que me toca hacer una chapuza para saber la largária del ArrayList*/
+            
+            int indiceMax = 0;
+            
+            for (String trampa : alumnos) {
+                indiceMax++;
+            }
+
+            if (indice-1 > indiceMax-1 || indice-1 < 0) {
+                
+                System.out.println("A la proxima introduce un número dentro del rango.");
+
+            } else {
+
+                System.out.println(alumnos.get(indice-1));
+
+            }
+            
+        }
 
     }
 
