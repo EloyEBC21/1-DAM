ca46e21caba39ef0320e0fdb3d0ee17ba8ca500f
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2026-01-07 20:36:17 +0100

    Hol

--- Programación/2do_Trimestre/PruebaPostNavidad/PostNavidad.java
+++ Programación/2do_Trimestre/PruebaPostNavidad/PostNavidad.java
@@ -6,8 +6,9 @@ public class PostNavidad{
 
     public static void main(String[] args) {
         
-        cuadrado();
-        triangulo();
+        //cuadrado();
+        //triangulo();
+        ajedrez();
         
     }
 
@@ -116,6 +117,220 @@ public class PostNavidad{
 
         }
     
+    }
+
+    public static void ajedrez(){
+
+        String[][] tablero = new String[10][10];
+        String Jugador1;
+        String Jugador2;
+        
+        int contador=0;
+
+        //mostrarTablero(tablero);
+
+        System.out.println();
+        System.out.println("Introduce el nombre de el/la Primer/a jugador/a:");
+        Jugador1=sc.next();
+
+        System.out.println();
+        System.out.println("Introduce el nombre de el/la Segundo/a jugador/a:");
+        Jugador2=sc.next();
+
+        System.out.println();
+        System.out.println(Jugador1 + " vs " + Jugador2);
+
+        do{
+
+            contador++;
+
+            if (contador%2==1) {
+                System.out.println();
+                System.out.println("--------- Turno de " + Jugador1 + " ---------");
+                System.out.println();
+            } else if (contador%2==0) {
+                System.out.println();
+                System.out.println("--------- Turno de " + Jugador2 + " ---------");
+                System.out.println();
+            }
+            
+            mostrarTablero(tablero);
+
+            jugadaTablero(tablero, contador);
+
+            break;
+
+        }while(true);
+
+    }
+
+    public static void mostrarTablero(String[][] tablero){
+
+        /* Programamos el tablero base */
+
+        /* Ponemos huecos vacios para rellenar la tabla */
+
+        tablero[0][0] = "   ";
+        tablero[0][9] = "   ";
+        tablero[9][0] = "   ";
+        tablero[9][9] = "   ";
+
+            /* Primero ponemos las letras/numeros para ayudar a localizar las piezas */
+
+        for (int i = 0; i <= tablero.length; i++) {
+            if (i==0 || i==tablero.length-1) {
+                
+                tablero[i][1] = " a ";
+                tablero[i][2] = " b ";
+                tablero[i][3] = " c ";
+                tablero[i][4] = " d ";
+                tablero[i][5] = " e ";
+                tablero[i][6] = " f ";
+                tablero[i][7] = " g ";
+                tablero[i][8] = " h ";
+            
+            } 
+        }
+
+        for (int i = 0; i <= tablero.length; i++) {
+            if (i==0 || i==tablero.length-1) {
+                
+                tablero[1][i] = " 1 ";
+                tablero[2][i] = " 2 ";
+                tablero[3][i] = " 3 ";
+                tablero[4][i] = " 4 ";
+                tablero[5][i] = " 5 ";
+                tablero[6][i] = " 6 ";
+                tablero[7][i] = " 7 ";
+                tablero[8][i] = " 8 ";
+            
+            } 
+        }
+
+        /*Ahora pondremos las fichas en el tablero */
+
+        /* Bando blanco */
+
+            /* Torres */
+        tablero[1][1] = " \u2656 ";
+        tablero[1][8] = " \u2656 ";
+
+            /* Caballos */
+        tablero[1][2] = " \u2658 ";
+        tablero[1][7] = " \u2658 ";
+
+            /* Alfiles */
+        tablero[1][3] = " \u2657 ";
+        tablero[1][6] = " \u2657 ";
+
+            /* Reina */
+        tablero[1][5] = " \u2655 ";
+
+            /* Rey */
+        tablero[1][4] = " \u2654 ";
+
+        /* Creamos un For para colocar los peones */
+
+        for (int i = 1; i <= 8; i++) {
+            tablero[2][i]=" \u2659 ";
+        }
+
+        /* Y el bando negro */
+
+            /* Torres */
+        tablero[8][1] = " \u265C ";
+        tablero[8][8] = " \u265C ";
+
+            /* Caballos */
+        tablero[8][2] = " \u265E ";
+        tablero[8][7] = " \u265E ";
+
+            /* Alfiles */
+        tablero[8][3] = " \u265D ";
+        tablero[8][6] = " \u265D ";
+
+            /* Reina */
+        tablero[8][4] = " \u265B ";
+
+            /* Rey */
+        tablero[8][5] = " \u265A ";
+
+
+            /* Peones */
+        for (int i = 1; i <= 8; i++) {
+            tablero[7][i]=" \u265F ";
+        }
+
+        /* Para terminar, rellenaremos los espacioes vacios de la matríz con espacios en blanco o con algún signo */
+
+        for (int i = 3; i < 7; i++) {
+            for (int j = 1; j < tablero.length-1; j++) {
+                tablero[i][j] = "   ";
+            }
+        }
+
+        /* Iteramos con 2 for nuestro tablero */
+
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j]);
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void jugadaTablero(String[][] tablero,int contador){
+
+        String posPieza = "";
+        String posMov;
+        int col = 0;
+        int fila = 0;
+
+        System.out.println();
+        System.out.println("Selecciona la pieza que desees mover:");
+        System.out.println("Ej: A1 , B5 , G7 , C4");
+        System.out.println();
+        
+        posPieza=sc.next();
+        fila = Integer.parseInt(posPieza.split("")[1]);
+
+        switch (posPieza.split("")[0]) {
+            case "A":
+                col = 1;
+                break;
+            case "B":
+                col = 2;
+                break;
+            case "C":
+                col = 3;
+                break;
+            case "D":
+                col = 4;
+                break;
+            case "E":
+                col = 5;
+                break;
+            case "F":
+                col = 6;
+                break;
+            case "G":
+                col = 7;
+                break;
+            case "H":
+                col = 8;
+                break;
+            default:
+                System.out.println("Introduce un valor como los dichos previamente en el ejemplo.");
+                break;
+        }
+
+        /* Cambiar ahora mas adelante */
+
+        if (tablero[col][fila] == "   ") {
+            
+        }
+
     }
 
 }
