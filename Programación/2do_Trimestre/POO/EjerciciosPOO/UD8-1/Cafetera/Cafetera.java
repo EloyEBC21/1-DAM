a546328c104395d4bafffd45496f418519a875f6
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2026-01-21 19:21:54 +0100

    Programación Cafetera

---
+++ Programación/2do_Trimestre/POO/EjerciciosPOO/UD8-1/Cafetera/Cafetera.java
@@ -0,0 +1,31 @@
+public class Cafetera {
+
+    int capMax, cantAct;
+
+    public Cafetera(){ this.capMax = 100; this.capMax=0;}
+
+    public Cafetera(int capMax){ this.cantAct=capMax; this.capMax=capMax;}
+
+    public Cafetera(int capMax, int cantAct){ if (cantAct > capMax){this.cantAct=capMax;this.capMax=cantAct;}else{this.cantAct=cantAct; this.capMax = capMax;}}
+    
+    public int getCapMax() {
+        return capMax;
+    }
+
+    public void setCapMax(int capMax) {
+        this.capMax = capMax;
+    }
+
+    public int getCantAct() {
+        return cantAct;
+    }
+
+    public void setCantAct(int cantAct) {
+        this.cantAct = cantAct;
+    }
+
+    public void llenarCafetera(){ this.cantAct = capMax;}
+
+    public void servirTaza(int cantidad){ this.cantAct -= cantidad;}
+
+}
