c0886d581891852f705b9282f36c22e3b9b706a0
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2026-01-29 18:15:51 +0100

    Programación Visto bueno

--- Programación/2do_Trimestre/Actividad_IA/AnalizadorSecuencias.java
+++ Programación/2do_Trimestre/Actividad_IA/AnalizadorSecuencias.java
@@ -4,40 +4,43 @@ public class AnalizadorSecuencias {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        /* Pedimos al usuario el número de casos de prueba */
+        /* Pedimos el número de casos inicial */
         int casos = teclado.nextInt();
+        
+        /* Usamos una variable para ir guardando todos los resultados y mostrarlos al final */
+        String resultadosFinales = "";
 
-        /* Se iterará el programa las veces introducidas por el usuario */
+        /* Bucle para procesar cada caso */
         for (int i = 0; i < casos; i++) {
             int contadorMayor = 0;
             int contadorMenor = 0;
 
-            /* Pedimos la cantidad de números que tendrá esta línea */
+            /* Pedimos la cantidad de números de la secuencia actual */
             int cantidadNumeros = teclado.nextInt();
 
-            /* Si el usuario dice que habrá al menos un número, empezamos a comparar */
             if (cantidadNumeros > 0) {
                 int numeroActual = teclado.nextInt();
 
-                /* Analizamos el resto de números de la línea */
                 for (int j = 1; j < cantidadNumeros; j++) {
                     int numeroSiguiente = teclado.nextInt();
 
-                    /* Comparación lógica según el pseudocódigo */
                     if (numeroSiguiente > numeroActual) {
                         contadorMayor++;
                     } else if (numeroSiguiente < numeroActual) {
                         contadorMenor++;
                     }
-
-                    /* Actualizamos el número actual para la siguiente comparación */
+                    /* El siguiente se convierte en el actual para la próxima comparación */
                     numeroActual = numeroSiguiente;
                 }
             }
 
-            /* Mostramos los resultados de la línea analizada */
-            System.out.println("Mayores: " + contadorMayor + " - Menores: " + contadorMenor);
+            /* Acumulamos el resultado de este caso en nuestra variable de texto */
+            resultadosFinales += "Mayores: " + contadorMayor + " - Menores: " + contadorMenor + "\n";
         }
+
+        /* Una vez terminados todos los casos, mostramos todo el bloque de resultados */
+        System.out.println("\n--- RESULTADOS FINALES ---");
+        System.out.print(resultadosFinales);
 
         teclado.close();
     }
