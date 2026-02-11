07e0587281497d7f4332e3a22dd869b8606b6971
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2026-02-11 15:49:23 +0100

    Programación Realicado Cafetera

--- Programación/2do_Trimestre/POO/EjerciciosPOO/Act8.1/Cafetera/Cafetera.java
+++ Programación/2do_Trimestre/POO/EjerciciosPOO/Act8.1/Cafetera/Cafetera.java
@@ -2,11 +2,28 @@ public class Cafetera {
 
     int capMax, cantAct;
 
-    public Cafetera(){ this.capMax = 100; this.capMax=0;}
+    public Cafetera(){ 
+        /* Constructor Base para definir la capaciad Máxima y cantidad Actual */
+        this.capMax = 100;
+        this.cantAct= 50;
+    }
 
-    public Cafetera(int capMax){ this.cantAct=capMax; this.capMax=capMax;}
+    public Cafetera(int capMax){
+        this.cantAct=capMax; 
+        this.capMax=capMax;
+    }
 
-    public Cafetera(int capMax, int cantAct){ if (cantAct > capMax){this.cantAct=capMax;this.capMax=cantAct;}else{this.cantAct=cantAct; this.capMax = capMax;}}
+    public Cafetera(int capMax, int cantAct){
+
+        if (cantAct > capMax){
+            this.cantAct=capMax;
+            this.capMax=cantAct;
+        } else {
+            this.cantAct=cantAct;
+            this.capMax = capMax;
+        }
+
+    }
     
     public int getCapMax() {
         return capMax;
@@ -24,8 +41,20 @@ public class Cafetera {
         this.cantAct = cantAct;
     }
 
-    public void llenarCafetera(){ this.cantAct = capMax;}
+    public void llenarCafetera(){ 
+        this.cantAct = capMax;
+    }
 
-    public void servirTaza(int cantidad){ this.cantAct -= cantidad;}
+    public void servirTaza(int cantidad){
+        this.cantAct -= cantidad;
+    }
+
+    public void vaciarCafetera(){
+        this.cantAct = 0;
+    }
+
+    public void agregarCafe(int cantidad){
+        this.cantAct += cantidad;
+    }
 
 }
--- Programación/2do_Trimestre/POO/EjerciciosPOO/Act8.1/Cafetera/appCafetera.java
+++ Programación/2do_Trimestre/POO/EjerciciosPOO/Act8.1/Cafetera/appCafetera.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class appCafetera{
 
+    public static Cafetera cafe = new Cafetera();
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
     
-        
+        System.out.println();
+        System.out.println("La cantidad actual es: " + cafe.getCantAct());
+        System.out.println("La capacidad maxima es: " + cafe.getCapMax());
+        System.out.println();
+ 
+        cafe.vaciarCafetera();
+
+        System.out.println("La cantidad actual despues del vacio: " + cafe.getCantAct());
+        System.out.println();
+
+        cafe.llenarCafetera();
+
+        System.out.println("La cantidad actual despues de llenarla: " + cafe.getCantAct());
+        System.out.println();
+
+        cafe.vaciarCafetera();
+
+        while (true) {
+
+            System.out.println("Introduce la cantidad que desees agregar a la cafetera:");
+            int cantidad = sc.nextInt();
+            int comprobamos = cafe.getCantAct() + cantidad;
+
+            if (comprobamos<=cafe.capMax) {
+                cafe.agregarCafe(cantidad);
+                break;
+            } else { System.out.println("Has inttroducido demasiado cafe, introduce menos. La cantidad actual es: " + cafe.cantAct + " y la cafetera soporta: " + cafe.capMax);}
+        }
+
+        System.out.println("Cantidad Actualizada: " + cafe.getCantAct() + " granos de café añadidos.");
 
     }
     
