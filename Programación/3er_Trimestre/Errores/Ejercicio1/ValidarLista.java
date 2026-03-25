a9e39e0c9db0966d015a8da2103a1e8d97854e02
Eloy Ballester Chust <eloyballesetr2@gmail.com>
2026-03-25 16:35:27 +0100

    Programacion Inicio Validar listas

---
+++ Programación/3er_Trimestre/Errores/Ejercicio1/ValidarLista.java
@@ -0,0 +1,11 @@
+public class ValidarLista{
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+    public static final String[] COMPOSITORES = {"Bach", "Haydn", "Mozart", "Beethoven","Brahms", "Mahler", "Bartok"};
+
+}
